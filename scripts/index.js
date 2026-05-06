57a763775272c2c382f09e3ff8d55e50c5436286
Michael Ribeiro <michael-ribeiro-fs@users.noreply.github.com>
2026-05-06 11:47:51 -0400

    feat: implement modal profile edit functionality with reusable open/close functions

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,46 @@ let initialCards = [
 initialCards.forEach(function (item) {
   console.log(item.name);
 });
+
+const editPopup = document.querySelector("#edit-popup");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__title");
+const profileAbout = document.querySelector(".profile__description");
+
+const editForm = document.querySelector("#edit-profile-form");
+const nameInput = editPopup.querySelector(".popup__input_type_name");
+const aboutInput = editPopup.querySelector(".popup__input_type_description");
+const closeButton = editPopup.querySelector(".popup__close");
+
+profileEditButton.addEventListener("click", handleOpenEditModal);
+
+closeButton.addEventListener("click", () => closeModal(editPopup));
+
+editForm.addEventListener("submit", handleProfileFormSubmit);
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editPopup);
+}
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
+
+  closeModal(editPopup);
+}
